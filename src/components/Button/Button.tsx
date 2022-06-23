d39b60219e1614a80002bd49e89da87ae4970558
Helton Reis <hrkings@rhaizzing.com>
2022-06-23 17:49:27 -0300

    [refactor] Clean up style components

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -8,6 +8,41 @@ interface ButtonProps {
   onClick?: () => void;
 }
 
+function getSizeCss(size?: string) {
+  switch(size) {
+    case "small":
+      return `
+      font-size: 12px;
+      padding: 10px 16px;`;
+
+    case "large":
+      return `
+      font-size: 16px;
+      padding: 12px 24px;`;
+
+    default:
+      return `
+      font-size: 14px;
+      padding: 11px 20px;
+      `;
+  }
+}
+
+function getTypeCss(isPrimary?: boolean) {
+  if (isPrimary) {
+    return `
+    color: white;
+    background-color: #1ea7fd;
+    `;
+  }
+
+  return `
+  color: #333;
+  background-color: transparent;
+  box-shadow: rgba(0, 0, 0, 0.15) 0px 0px 0px 1px inset;
+  `;
+}
+
 const StyledButton = styled.button`
 font-family: 'Nunito Sans', 'Helvetica Neue', Helvetica, Arial, sans-serif;
 font-weight: 700;
@@ -17,50 +52,18 @@ cursor: pointer;
 display: inline-block;
 line-height: 1;
 
-color: ${(props: ButtonProps) => props.primary ? "white" : "#333"};
-background-color: ${(props: ButtonProps) => props.primary ? "#1ea7fd" : "transparent"};
-${(props: ButtonProps) => props.primary ? "" : "box-shadow: rgba(0, 0, 0, 0.15) 0px 0px 0px 1px inset;"}
-
+${(props: ButtonProps) => getTypeCss(props.primary)}
+${(props: ButtonProps) =>  getSizeCss(props.size)}
 ${(props: ButtonProps) => props.backgroundColor ? `background-color: ${props.backgroundColor};` : ""}
-
-font-size: 14px;
-padding: 11px 20px;
 `;
-
-const SmallStyledButton = styled(StyledButton)`
-font-size: 12px;
-padding: 10px 16px;
-`
-
-const LargeStyledButton = styled(StyledButton)`
-font-size: 16px;
-padding: 12px 24px;
-`
 
 /**
  * Primary UI component for user interaction
  */
 export const Button = (props : ButtonProps) => {
-  switch (props.size) {
-    case "small":
-      return (
-        <SmallStyledButton {...props}>
-          {props.label}
-        </SmallStyledButton>
-      );
-
-    case "large":
-      return(
-        <LargeStyledButton {...props}>
-          {props.label}
-        </LargeStyledButton>
-      );
-
-    default:
-      return(
-        <StyledButton {...props}>
-          {props.label}
-        </StyledButton>
-      );
-  }
+  return (
+    <StyledButton {...props}>
+      {props.label}
+    </StyledButton>
+  );
 };
